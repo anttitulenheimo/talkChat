577491982a094826e5610d3154e2948a79ada196
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-08-22 10:44:02 +0300

    Refactored backend for tests and created first tests for user behavior.

--- talkChat_backend/index.js
+++ talkChat_backend/index.js
@@ -1,39 +1,13 @@
-const express = require('express')
 const http = require('http')
 const { Server } = require('socket.io')
-const cors = require('cors')
-
-// ROUTERS
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
-const messageRouter = require('./controllers/messages')
-const chatRouter = require('./controllers/chats')
 
 const mongoose  = require('mongoose')
 const config = require('./utils/config')
 
 const Message = require('./models/message')
 
+const app = require('./app')
 
-
-const app = express()
-app.use(express.json())
-app.use(cors())
-
-
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
-app.use('/api/messages', messageRouter)
-app.use('/api/chats', chatRouter)
-
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch ((error) => {
-    console.log(`Error connecting to MongoDB\nmessage: ${error.message}`)
-  })
 
 
 const httpServer = http.createServer(app)
---
+++ talkChat_backend/tests/talkchat_api.test.js
@@ -0,0 +1,86 @@
+const { test, after, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const User = require('../models/user')
+
+
+const api = supertest(app)
+
+const constantTestUser = {
+    username: 'Deus',
+    name: 'Maximus Decimus Meridius',
+    password: 'easy'
+}
+beforeEach(async () => {
+    await User.deleteMany({})
+    
+})
+
+describe('USERTEST1: A new user can be created and the data is send to database', () => {
+
+    test('user can be created', async () => {
+
+    await api
+        .post('/api/users/')
+        .send(constantTestUser)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    })
+})
+
+describe('USERTEST2: A new user is created and the user can login', () => {
+
+    test('Created user can login', async () => {
+
+    await api
+        .post('/api/users/')
+        .send(constantTestUser)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    await api
+        .post('/api/login/')
+        .send({username: constantTestUser.username, password: constantTestUser.password})
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+})
+describe('USERTEST3: User can create a new chat with another user', () => {
+
+    test('Create a new chat', async () => {
+
+        await api   // CREATE USER#1
+            .post('/api/users/')
+            .send(constantTestUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        
+        await api   // CREATE USER#2
+            .post('/api/users/')
+            .send({username: "AnotherUser", name: "Your neighbour David", password: 'abc123'})
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const responseUserOne = await api
+            .get(`/api/users/username/${constantTestUser.username}`)
+            .expect(200)
+
+        const responseUserTwo = await api 
+            .get(`/api/users/username/${"AnotherUser"}`)
+            .expect(200)
+
+        await api   // Creating a POST request for a new chat
+            .post('/api/chats')
+            .send({senderId: responseUserOne.body.id, receiverId: responseUserTwo.body.id})
+            .expect(201)
+    })
+  
+})
+
+
+
+after(async () => {
+  await mongoose.connection.close()
+})
