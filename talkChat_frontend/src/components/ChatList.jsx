48a2435a46553487c3a2dba8b91582ef9c968982
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-08-26 20:33:42 +0300

    ADD: Delete button with some styling on the side.

--- talkChat_frontend/src/components/ChatList.jsx
+++ talkChat_frontend/src/components/ChatList.jsx
@@ -3,7 +3,8 @@ import messageService from "../services/messageService.js"
 import userService from "../services/userService.js"
 import chatService from "../services/chatService.js"
 import ChatDisplay from "./ChatDisplay.jsx";
-import { Card, CardContent, List, ListItemButton, ListItemAvatar, Avatar, Typography, Divider, Box, Button } from "@mui/material";
+import { Card, CardContent, List, ListItemButton, ListItemAvatar, ListItem, Avatar, Typography, Divider, Box, Button } from "@mui/material";
+import ThreeDotsMenu from './ThreeDotsMenu.jsx'
 
 import _ from "lodash";
 
@@ -145,37 +146,48 @@ const ChatList = ({ userId, addMessage, username, socket }) => {
           : text
     }
 
-
     const chatListComponent = () => (
         <Card>
             <CardContent>
-                <Typography variant="h6" gutterBottom>Chats</Typography>
-                <List>
-                    {chats.map(chat => (
-                        <div key={chat.id}>
-                            <ListItemButton onClick={(event) => handleUsernameClick(event, chat.id)}>
-                                <ListItemAvatar>
-                                    <Avatar>{chat.anotherUserName[0].toUpperCase()}</Avatar>
-                                </ListItemAvatar>
-                                <Box>
-                                    <Typography>{chat.anotherUserName}</Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        {truncate(latestMessages[chat.id])|| "No messages yet"}
-                                    </Typography>
-                                </Box>
-                            </ListItemButton>
-                            <Divider />
-                        </div>
-                    ))}
+            <Typography variant="h6" gutterBottom>Chats</Typography>
+            <List>
+                {chats.map(chat => (
+                <div key={chat.id}>
+                    <ListItem
+                    sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", px: 0 }}
+                    >
+                    {/* Left: clickable chat info */}
+                    <ListItemButton
+                        sx={{ flexGrow: 1, width: "100%"}}
+                        onClick={(event) => handleUsernameClick(event, chat.id)}
+                    >
+                        <ListItemAvatar>
+                        <Avatar>{chat.anotherUserName[0].toUpperCase()}</Avatar>
+                        </ListItemAvatar>
+                        <Box>
+                        <Typography>{chat.anotherUserName}</Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {truncate(latestMessages[chat.id]) || "No messages yet"}
+                        </Typography>
+                        </Box>
+                    </ListItemButton>
 
-                        {!loadingChats && chats.length === 0 && (
-                            <Typography variant="body2" color="text.secondary">No chats available</Typography>
-                        )}
+                    {/* Right: three dots */}
+                    <ThreeDotsMenu chatId={chat.id} chats={chats} setChats={setChats} />
+                    </ListItem>
+                    <Divider />
+                </div>
+                ))}
 
-                </List>
+                {chats.length === 0 && (
+                <Typography variant="body2" color="text.secondary">
+                    No chats available
+                </Typography>
+                )}
+            </List>
             </CardContent>
         </Card>
-    )
+);
 
     const chatDisplayComponent = () => (
         <Box>
---
+++ talkChat_frontend/src/components/ThreeDotsMenu.jsx
@@ -0,0 +1,147 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
+
+// Service
+import chatService from '../services/chatService'
+
+export default function ThreeDotsMenu({ chatId, chats, setChats }) {
+
+  const idForDeletion = chatId
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  }
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  }
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  }
+
+  const handleChatDelete = (event) => {
+    event.preventDefault()
+    
+    console.log('HEY! you are pressing the delete button!')
+    console.log(idForDeletion)
+    
+   if (window.confirm('Press OK to delete chat')) {
+    try {
+        chatService.remove(chatId)
+        console.log(chatId)
+        console.log('Chat deleted succesfully')
+        setAnchorEl(null);
+        handleMobileMenuClose()
+        setChats(chats.filter(chat => chat.id !== idForDeletion))
+    } catch (e) {
+        console.log(e.message)
+      }
+  }
+}
+
+  const menuId = 'quick-settings-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleChatDelete}>delete</MenuItem>
+    </Menu>
+  )
+
+  const mobileMenuId = 'quick-settings-menu-mobile';
+  const renderMobileMenu = (
+  <Menu
+    anchorEl={mobileMoreAnchorEl}
+    anchorOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    id={mobileMenuId}
+    keepMounted
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    open={isMobileMenuOpen}
+    onClose={handleMobileMenuClose}
+  >
+    <MenuItem onClick={(event) => { handleChatDelete(event); handleMobileMenuClose(); }}>
+      <IconButton
+        size="large"
+        aria-label="delete chat"
+        color="inherit"
+      >
+
+      </IconButton>
+      <p>Delete</p>
+    </MenuItem>
+  </Menu>
+);
+
+  return (
+
+    <Box sx={{ flexGrow: 1 }}>
+        <Toolbar>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <MoreVertIcon />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="small"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreVertIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  )
+}
+
